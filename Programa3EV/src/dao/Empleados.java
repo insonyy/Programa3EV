e70b7339ee0fd9d9bdf56f49696ca3db20214f12
Sonia <sonia.sanchez6@educa.madrid.org>
2023-04-28 10:55:07 +0200

    empleado

--- Programa3EV/src/dao/Empleados.java
+++ Programa3EV/src/dao/Empleados.java
@@ -1,5 +1,16 @@
 package dao;
 
-public class Empleados {
+import java.util.ArrayList;
+import java.util.List;
 
+import domain.Empleado;
+
+public class Empleados {
+	private List<Empleado> empleados;
+	
+	public void Empleados() {
+		empleados=new ArrayList();
+		
+		
+	}
 }
--- Programa3EV/src/domain/Empleado.java
+++ Programa3EV/src/domain/Empleado.java
@@ -5,6 +5,13 @@ import lombok.Data;
 @Data
 public class Empleado {
 	private String nombre;
-	private String dNI;
+	private String dni;
 	private String cargo;
+	//Constructor
+	public Empleado(String nombre, String dni, String cargo) {
+		this.nombre = nombre;
+		this.dni = dni;
+		this.cargo = cargo;
+	}
+	
 }
