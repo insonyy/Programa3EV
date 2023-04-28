46f5796cacddc5bb7e616fbea88f0b804d76b8ef
Sonia <sonia.sanchez6@educa.madrid.org>
2023-04-28 12:29:19 +0200

    sonia ajskdnlaksdna

--- Programa3EV/src/common/Constantes.java
+++ Programa3EV/src/common/Constantes.java
@@ -1,4 +1,30 @@
 package common;
 
 public class Constantes {
+	/*	
+	 		System.out.println("Seleccione una de las siguientes opciones:");
+			System.out.println("1.Añadir empleados");
+			System.out.println("2.Modificar información sobre empleados");
+			System.out.println("3.Eliminar ficha empleados");
+			System.out.println("4.Nuevo paciente");
+			System.out.println("5.Modificar informacion de paciente");
+			System.out.println("6.Eliminar ficha paciente");
+			System.out.println("7.Añadir tratamiento a paciente");
+			System.out.println("8.Modificar tratamiento de paciente");
+			System.out.println("9.Suspender tratamiento de paciente");
+			System.out.println("10.Salir");
+			System.out.println("Solo números entre 1 y 10");
+			System.out.println("Seleccione que desea hacer:");
+			System.out.println("1. Listar pacientes");
+			System.out.println("2. Listar empleados");
+			System.out.println("3. Listar ingresos actuales");
+			System.out.println("4. Listar paciente por especie");
+			System.out.println("5. Listar empleados por cargo");
+			System.out.println("6. Listar tratamientos en curso");
+			System.out.println("7. Acceder a gestion de datos");
+			System.out.println("8. Salir");
+			System.out.println("Por favor introduzca contraseña para acceder a la gestion de datos");
+			System.out.println("Contraseña incorrecta");
+			System.out.println("Solo números entre 1 y 8");
+	*/
 }
