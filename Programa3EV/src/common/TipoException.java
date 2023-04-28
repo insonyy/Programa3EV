57f048c79460e0bc8420bec6a24674ba64a9f941
¡ <yeetyhaaw@gmail.com>
2023-04-28 10:05:43 +0200

    10:05 28/04/2023

--- Programa3EV/src/common/TipoException.java
+++ Programa3EV/src/common/TipoException.java
@@ -1,6 +1,11 @@
 package common;
 
-public class TipoException {
+public class TipoException extends Exception {
 
-    
+/*Constructores*/
+
+    public TipoException() {
+
+        super("El tipo introducido no corresponde con los disponibles.");
+    }
 }
--- Programa3EV/src/domain/Exoticos.java
+++
@@ -1,4 +0,0 @@
-package domain;
-
-public class Exoticos extends Animales{
-}
