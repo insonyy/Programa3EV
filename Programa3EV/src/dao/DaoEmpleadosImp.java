9df6ccaf377a869ca489d4e40f2d148d67f7f55e
SrMikelsius <120173704+SrMikelsius@users.noreply.github.com>
2023-05-04 11:11:02 +0200

    Emp

--- Programa3EV/src/dao/DaoEmpleadosImp.java
+++ Programa3EV/src/dao/DaoEmpleadosImp.java
@@ -1,10 +1,13 @@
 package dao;
 
 import java.util.List;
+import java.util.Scanner;
 
 import domain.Empleado;
 
 public class DaoEmpleadosImp implements DaoEmpleados{
+
+	protected final Empleados lista;
 
 	@Override
 	public List<Empleado> getListaEmpleados() {
@@ -18,10 +21,20 @@ public class DaoEmpleadosImp implements DaoEmpleados{
 		return false;
 	}
 
+
+
 	@Override
-	public boolean modificarEmpleado(Empleado empleado) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean modificarEmpleado() {
+			Scanner sc = new Scanner(System.in);
+			boolean nuevo = true;
+			System.out.println("Introduce el nombre del empleado:");
+			String nombre = sc.nextLine();
+			Empleado empleado = (Empleado) lista.getEmpleados().stream().filter(empleado1 -> empleado1.getCargo().equalsIgnoreCase(nombre));
+			System.out.println("¿Cual es su nuevo cargo?");
+			String cargo = sc.nextLine();
+			empleado.setCargo(cargo);
+			return false;
+
 	}
 
 	@Override
