755259be2259ded8c119fdf7ce4c56ab202934a6
Sonia <sonia.sanchez6@educa.madrid.org>
2023-05-05 12:12:52 +0200

    menuconstantes

--- Programa3EV/src/ui/Main.java
+++ Programa3EV/src/ui/Main.java
@@ -1,12 +1,10 @@
 package ui;
 
-import dao.Animales;
-
 public class Main {
 
 	public static void main(String[] args) {
 		MenuPrincipal mPrincipal = new MenuPrincipal();
-		mPrincipal.menuPrincipal(new MenuGestion());
+		mPrincipal.menuPrincipal();
 
 	}
 
