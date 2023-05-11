643c13fb32b9122588fb885c0dd98896fe2ad436
¡ <yeetyhaaw@gmail.com>
2023-05-11 13:04:42 +0200

    13:04 11/05/2023

---
+++ Programa3EV/src/domain/Ingreso.java
@@ -0,0 +1,10 @@
+package domain;
+
+import java.time.LocalDate;
+
+public class Ingreso {
+    private LocalDate fecha;
+    private String nombreAnimal;
+    private String nifEmpleado;
+    private String tratamiento;
+}
