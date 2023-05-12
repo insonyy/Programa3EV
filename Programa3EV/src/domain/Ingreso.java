2f5045ab09e0db5c770e77aa1199f61fd9426c69
SrMikelsius <120173704+SrMikelsius@users.noreply.github.com>
2023-05-12 13:23:49 +0200

    Ingresos

--- Programa3EV/src/domain/Ingreso.java
+++ Programa3EV/src/domain/Ingreso.java
@@ -1,27 +1,20 @@
-//package domain;
-//
-//import java.time.LocalDate;
-//import java.util.HashMap;
-//
-//public class Ingreso {
-//    private LocalDate fecha;
-//    private String nombreAnimal;
-//    private String nifEmpleado;
-//    private String tratamiento;
-//
-//    public Ingreso(LocalDate fecha, String nombreAnimal, String nifEmpleado, String tratamiento) {
-//        this.fecha = fecha;
-//        this.nombreAnimal = nombreAnimal;
-//        this.nifEmpleado = nifEmpleado;
-//        this.tratamiento = tratamiento;
-//    }
-//
-//    public void map() {
-//        HashMap<Integer, Empleado> map = new HashMap<>();
-//        for (int i = 0; i < getListaEmpleados().size(); i++) {
-//            map.put(i, getListaEmpleados().get(i));
-//            System.out.println(map.get(i).toString());
-//
-//        }
-//    }
-//}
+package domain;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+
+public class Ingreso {
+    private LocalDate fecha;
+    private String nombreAnimal;
+    private String nifEmpleado;
+    private String tratamiento;
+
+    public Ingreso(LocalDate fecha, String nombreAnimal, String nifEmpleado, String tratamiento) {
+        this.fecha = fecha;
+        this.nombreAnimal = nombreAnimal;
+        this.nifEmpleado = nifEmpleado;
+        this.tratamiento = tratamiento;
+    }
+
+
+}
