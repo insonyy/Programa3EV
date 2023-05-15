340ee4bcf040d21d281fb598092c9763993776bc
inso <yeetyhaaw@gmail.com>
2023-05-15 11:56:18 +0200

    11:56 15/05/2023

---
+++ src/test/java/service/ServicioGestionTest.java
@@ -0,0 +1,72 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServicioGestionTest {
+
+    @Test
+    void getListaAnimales() {
+    }
+
+    @Test
+    void getListaAnimalesEspecie() {
+    }
+
+    @Test
+    void nuevoAnimal() {
+    }
+
+    @Test
+    void modificarAnimal() {
+    }
+
+    @Test
+    void eliminarFichaAnimal() {
+    }
+
+    @Test
+    void getListaEmpleados() {
+    }
+
+    @Test
+    void nuevoEmpleado() {
+    }
+
+    @Test
+    void modificarEmpleado() {
+    }
+
+    @Test
+    void eliminarFichaEmpleado() {
+    }
+
+    @Test
+    void getListaTratamientos() {
+    }
+
+    @Test
+    void nuevoTratamiento() {
+    }
+
+    @Test
+    void modificarTratamiento() {
+    }
+
+    @Test
+    void suspenderTratamiento() {
+    }
+
+    @Test
+    void listarEmpleadosCargo() {
+    }
+
+    @Test
+    void escribirFicheroEmpleado() {
+    }
+
+    @Test
+    void escribirFicheroAnimales() {
+    }
+}
